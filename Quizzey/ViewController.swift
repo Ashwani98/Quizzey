cadebab27c73691a9ba0eacf6a9486b6a2a47bbd
Ashwani <steveash98@gmail.com>
2018-07-14 11:58:26 +0530

    Initial Commit

---
+++ Quizzey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quizzey
+//
+//  Created by Ashwani  Agrawal on 14/07/18.
+//  Copyright © 2018 Ashwani  Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
